2f6e35a120b20ed7f18639ac000871060340e5e7
Doug Chang <dougchang@dougs-mbp.lan>
2019-06-19 04:11:47 -0700

    add

--- testarray/testmap_objects.js
+++ testarray/testmap_objects.js
@@ -98,20 +98,13 @@ Object.size = function(obj) {
 map = {};
 
 for (const k in profiles) {
-  //console.log(profiles[k]["favoriteMovieID"]);
-  //console.log(profiles[k]["favoriteMovieID"] in map);
   if (profiles[k]["favoriteMovieID"] in map == false) {
     map[profiles[k]["favoriteMovieID"]] = [];
     map[profiles[k]["favoriteMovieID"]].push(profiles[k]["userID"]);
   } else {
-    //console.log('map true');
     arr = map[profiles[k]["favoriteMovieID"]];
-    //console.log('arr contains:', arr);
-    //console.log('adding:', profiles[k]['userID']);
     arr.push(profiles[k]["userID"]);
-    //console.log('arr after add:', arr);
     map[profiles[k]["favoriteMovieID"]] = arr;
-    //console.log('after add:', map[profiles[k]['favoriteMovieID']]);
   }
 }
 
@@ -122,29 +115,21 @@ console.log("size users:", Object.size(users));
 console.log("size profiles:", Object.size(profiles));
 
 function lookup_uname(o) {
-  console.log("lookup_uname");
-  console.log("user::", users[o]["name"]);
   return "<li>" + users[o]["name"] + "</li>";
 }
 
 let movie_names = [];
 for (const k in movies) {
-  console.log(movies[k]["name"]);
-  console.log(movies[k]["id"]);
   s = "<h2>" + movies[k]["name"] + "</h2>";
   movie_names.push(s);
   arr = map[movies[k]["id"]];
-  console.log("arr:", arr, " typeof arr:", typeof arr);
   if (typeof arr != "undefined") {
     movie_names.push("<p>Liked By:</p><ul>");
     n = arr.map(x => lookup_uname(x));
-    console.log("n:", n);
     movie_names.push(n + "</ul>");
   } else {
-    console.log("undefined no favorites");
     movie_names.push("<p>None of the current users liked this movie</p>");
   }
-  console.log("movie_names after push:", movie_names);
   //
 }
 
