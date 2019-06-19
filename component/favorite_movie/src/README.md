be7ca1114940d3390d31b7b29ea66b25cfa0c692
Doug Chang <dougchang@dougs-mbp.lan>
2019-06-18 21:12:34 -0700

    add

--- component/favorite_movie/src/README.md
+++ component/favorite_movie/src/README.md
@@ -1,3 +1,5 @@
 old_index.js, display list w/ key set and with li. no ol tags needed!
+JS string concat not needed in JSX, put {JS} strings
 
+ 
 
--- component/favorite_movie/src/index.js
+++ component/favorite_movie/src/index.js
@@ -8,10 +8,16 @@ import {movies,profiles,users} from './data'
 class App extends Component {
   render() {
     const user_names = Object.keys(users).map((x)=>(users[x]['name']));
+    let output = []
+    for (const x in Object.keys(profiles)){
+       console.log(users[profiles[x]['userID']]['name']+' favorite movie is: ', movies[profiles[x]['favoriteMovieID']]['name']);
+       output.push(<li>{users[profiles[x]['userID']]['name']}  favorite movie is:  {movies[profiles[x]['favoriteMovieID']]['name']}</li>);
+    }
+    //{user_names.map((x)=><li key={x}>{x}  favorite movie is </li>)}
     return (
       <div>
         <h2>Favorite Movies</h2>
-	{user_names.map((x)=><li key={x}>{x}</li>)}
+	{output}
       </div>
     );
   }
