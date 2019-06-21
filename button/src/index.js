04a5df7598d1bd582affb07642740f752d9bfa78
Doug Chang <dougchang@dougs-mbp.lan>
2019-06-20 18:43:28 -0700

    add

--- button/src/index.js
+++ button/src/index.js
@@ -1,31 +1,31 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  num_click = 0;
+  constructor(props) {
     super(props);
-    this.state = {isToggleOn:true};
-  
-  this.handleClick = this.handleClick.bind(this);
+    this.state = { isToggleOn: true };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(){
-    this.setState(state=>({
+  handleClick() {
+    this.setState(state => ({
       isToggleOn: !state.isToggleOn
     }));
+    this.num_click++;
   }
 
-  render(){
-    return(
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? "ON":"OFF"}
-      </button>
-   );
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? "ON" + { tihs.num_click } : "OFF"}
+        </button>
+      </div>
+    );
   }
-
 }
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
--- component/func_comp/README.md
+++ component/func_comp/README.md
@@ -3,5 +3,8 @@ functional components dont inherit from Component
 funcional compoents dont use this but use props
 
 1) oldindex.js shows a functional component needs to have first capital letter
-2) refactor contactlist app to use functional components
+2) oldindexprops.js shows a functional component w/props. 
+3) refactor contactlist app to use functional components
 each component should be in a separate file like OO design
+4) oldindex3.js shows how to convert a functional component to a state component
+
