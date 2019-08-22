adf030ccf00503be69a5df3be3bf8ece944eae5f
Doug Chang <dougchang@Dougs-MacBook-Pro.local>
2019-08-22 16:34:11 -0700

    add

--- react_redux/first_redux/src/index.js
+++ react_redux/first_redux/src/index.js
@@ -1,7 +1,10 @@
 
 
 function createStore(reducer){
-  
+  //1. state
+  //2. get state
+  //3. listen to changes on state
+  //4. update state
   let state 
   listeners = []
 
@@ -19,19 +22,62 @@ function createStore(reducer){
   }
 
   return {
-    getState, sbuscribe,dispatch
+    getState, subscribe,dispatch
   }
 }
 
 
-
-
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
+}
 
 function todos(state=[], action){
-  if (action.type==='ADD_TODO')
-    return state.concat([action.todo])
-  return getState
+  switch (action.type){
+    case: 'ADD_TODO':
+      return state.concat([action.todo]);
+    case: 'REMOVE_TODO':
+      return state.filter((todo)=>todo.id!==action.id);
+    case: 'TOGGLE_TODO':
+      return state.map((todo)=> todo.id!==action.id ? todo :
+      Object.assign({},todo,{complete:!todo.complete}));
+    default:
+      return state
+  }
 }
+  
+    return state
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
+
+const store = createStore(app)
+
+store.subscribe(()=>{
+  console.log('new state;',store.getState())
+
+})
+
+store.dispatch({
+  type:'ADd_TODO',
+  todo: {
+    id:0,
+    name: 'redux',
+    complete:false
+  }
+})
 
 
 
