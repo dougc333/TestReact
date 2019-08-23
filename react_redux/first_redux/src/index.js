6702dfc6273259330b042e5b77dd2d3e14b2da53
Doug Chang <dougchang@Dougs-MacBook-Pro.local>
2019-08-23 15:32:23 -0700

    add

--- react_redux/first_redux/src/index.js
+++ react_redux/first_redux/src/index.js
@@ -9,6 +9,8 @@ function createStore(reducer){
   listeners = []
 
   const getState = ()=> state
+
+
   const subscribe = (listner)=>{
     listeners.push(listner)
     return ()=>{
@@ -26,6 +28,49 @@ function createStore(reducer){
   }
 }
 
+//Use consts instead of strings
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addToDoAction(todo){
+  return {
+    type:ADD_TODO,
+    todo
+  }
+}
+
+function removeToDoAction(id){
+  return{
+    type:REMOVE_TODO,
+    id
+  }
+}
+
+function toggleToDoAction(id){
+  return{
+    type:TOGGLE_TODO,
+    id
+  }
+}
+
+function addGoalAction(goal){
+  return{
+    type:ADD_GOAL,
+    goal
+  }
+}
+
+function removeGoalAction(id){
+  return{
+    type:REMOVE_GOAL,
+    id
+  }
+}
+
+
 
 function goals (state = [], action) {
   switch(action.type) {
@@ -40,20 +85,18 @@ function goals (state = [], action) {
 
 function todos(state=[], action){
   switch (action.type){
-    case: 'ADD_TODO':
+    case 'ADD_TODO':
       return state.concat([action.todo]);
-    case: 'REMOVE_TODO':
+    case 'REMOVE_TODO':
       return state.filter((todo)=>todo.id!==action.id);
-    case: 'TOGGLE_TODO':
+    case 'TOGGLE_TODO':
       return state.map((todo)=> todo.id!==action.id ? todo :
       Object.assign({},todo,{complete:!todo.complete}));
     default:
       return state
   }
 }
-  
-    return state
-}
+ 
 
 function app (state = {}, action) {
   return {
@@ -70,16 +113,24 @@ store.subscribe(()=>{
 
 })
 
-store.dispatch({
-  type:'ADd_TODO',
-  todo: {
-    id:0,
-    name: 'redux',
-    complete:false
-  }
-})
+store.dispatch(addToDoAction({
+  id:2,
+  name: 'Go gym',
+  complete:true
+}))
 
+store.dispatch(removeToDoAction(1))
 
+store.dispatch(toggleToDoAction(0))
 
+store.dispatch(addGoalAction({
+  id:0,
+  name:'learn redux'
+}))
 
+store.dispatch(addGoalAction({
+  id:1,
+  name:'lose weight too fat'
+}))
 
+store.dispatch(removeGoalAction(0))
---
+++ react_redux/first_redux/src/index4.js
@@ -0,0 +1,40 @@
+// Library Code
+function createStore (reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
+
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
+}
+
+// App Code
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
