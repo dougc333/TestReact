c9256206145330a534e41d28c05288fe6882b54a
Doug Chang <dougchang@Dougs-MacBook-Pro.local>
2019-08-26 11:20:45 -0700

    add

---
+++ react_redux/first_redux/src/run_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+conda activate js
+echo "index1.js"
+node index1.js
+echo "index2.js"
+node index2.js
+echo "index3.js"
+node index3.js
+echo "index4.js"
+node index4.js
+echo "index5.js"
+node index5.js
+echo "index6.js"
+node index6.js
+echo "index7.js"
+node index7.js
+echo "index8.js"
+node index8.js
+echo "index9.js"
+node index9.js
+echo "index1.0js"
+node index10.js
+echo "index11.js"
+node index11.js
+
